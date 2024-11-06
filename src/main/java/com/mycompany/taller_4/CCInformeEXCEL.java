c37048f762eb39cefb6bcfafa85cbd8a144cf1ca
Cesar Andre Mera Castro <cesar.mera.castro@gmail.com>
2024-11-06 17:59:27 -0500

    Clases del y metodos del Factory Method

---
+++ src/main/java/com/mycompany/taller_4/CCInformeEXCEL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller_4;
+
+/**
+ *
+ * @author cesar
+ */
+public class CCInformeEXCEL extends CreatorInforme{
+    public informeEXCEL crearInforme(String datos){
+        informeEXCEL informeExcel=new informeEXCEL();
+        return informeExcel;
+    }
+}
---
+++ src/main/java/com/mycompany/taller_4/informeEXCEL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller_4;
+
+/**
+ *
+ * @author cesar
+ */
+public class informeEXCEL implements Informe {
+    @Override
+    public void haceCosasDeInforme() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
