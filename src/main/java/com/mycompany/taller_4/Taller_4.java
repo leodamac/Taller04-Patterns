18018fa5cf0957ae97ae8e3deeee70191bf781cb
Cesar Andre Mera Castro <cesar.mera.castro@gmail.com>
2024-11-06 17:42:45 -0500

    Commit inicial

---
+++ src/main/java/com/mycompany/taller_4/Taller_4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller_4;
+
+/**
+ *
+ * @author cesar
+ */
+public class Taller_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
