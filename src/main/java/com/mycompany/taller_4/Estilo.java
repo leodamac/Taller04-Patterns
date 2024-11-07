ef3b134f91ba352512c768806295e8d7fcc7efba
Cesar Andre Mera Castro <cesar.mera.castro@gmail.com>
2024-11-06 22:09:06 -0500

    Clases y metodos

--- src/main/java/com/mycompany/taller_4/Estilo.java
+++ src/main/java/com/mycompany/taller_4/Estilo.java
@@ -1,15 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.taller_4;
 
 /**
  *
  * @author cesar
  */
-public class Taller_4 {
-    public static void main(String[] args) {
-       
+public class Estilo extends Decorator{
+    public void aggEstilo(){
+        
     }
 }
---
+++ src/main/java/com/mycompany/taller_4/Sistema.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller_4;
+
+/**
+ *
+ * @author cesar
+ */
+public class Sistema {
+    private Sistema instancia;
+    
+    private void Sistema(){
+        
+    }
+    public Sistema getInstancia(){
+        Sistema sistema=new Sistema();
+        
+        return sistema;
+    }
+    public void verReporte(){
+        
+    }
+    public void eliminarReporte(){
+        
+    }
+    public void crearReporte(String datos){
+        
+    }
+    
+}
